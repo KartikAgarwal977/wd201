72ceb7f454f59c9b40a15a49ff45abf5e43949ce
kartikagarwal977 <kartikag977@gmail.com>
2024-03-19 20:20:23 +0530

    kubernetes

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 30012;
 
 app.listen(PORT, () => {
   console.log(`Started express server at port ${PORT}`);
